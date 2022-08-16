11fa997e65806c58b7b844c3cf6430999b15d86b
Ryan Wang <rwryanwang@gmail.com>
2022-08-15 18:11:46 -0700

    Completed reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
 
+// Completed
+const reverseString = function(str) {
+    let arr = str.split('')
+                 .reverse();
+    
+    let newStr = arr.join('');
+
+    return newStr;
 };
 
 // Do not edit below this line
