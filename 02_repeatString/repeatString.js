31ed413084d2472dfe61a7dd109bb3389d5a1032
Ryan Wang <rwryanwang@gmail.com>
2022-08-15 18:06:59 -0700

    Completed repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+// Completed
+const repeatString = function(string, num) {
+    let str = "";
+   for (let i = 0; i < num; i++) {
+    str += string;
+    }
+    
+    return str;
 };
 
 // Do not edit below this line
