7a54d36058469cabdfd9eb3aa44f365f6964b46d
Ryan Wang <rwryanwang@gmail.com>
2022-08-16 12:22:50 -0700

    Completed leapYears.js; Need to plan steps better

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+// Completed
+const leapYears = function(year) {
+    
+if (year % 4 == 0 && year % 100 !== 0) {
+    return true;
+}
+if (year % 100 == 0) {
+    if (year % 400 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+else {
+    return false;
+}
 
+    
 };
 
+//console.log(leapYears(1900));
+console.log(leapYears(1600));
 // Do not edit below this line
 module.exports = leapYears;
