11e8fcb362e426e7729293a229c98d8bd3ea43b5
Ryan Wang <rwryanwang@gmail.com>
2022-08-16 11:23:19 -0700

    I suck; looked at solutions branch

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+//Cheated - I'm retarded
+const removeFromArray = function(arr, arg) {
 
+    let remove = Array.prototype.slice.call(arguments, 1);
+    let newArr = [];
+
+    newArr = arr.filter(x => !remove.includes(x));
+    console.log(newArr);
+    
+    return newArr;
+    
 };
 
+removeFromArray([1, 2, 3, 4], 4);
+//removeFromArray([1, 2, 3, 4], 3, 2);
+//removeFromArray([1, 2, 3], "1", 3);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
