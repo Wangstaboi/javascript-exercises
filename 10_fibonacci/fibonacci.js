eea07673a5ce4521024667253ee2c4e5520a9303
Ryan Wang <rwryanwang@gmail.com>
2022-08-25 16:51:05 -0700

    Finished fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
 
+//fibonacci = n + (n - 1)
+const fibonacci = function(num) {
+    let arr = [1, 1];
+
+    if (num > 0) {
+        for (let i = 1; i < num - 1; i++) {
+            let temp = i - 1;
+            arr.push(arr[i] + arr[temp]);
+        }
+        return arr[num - 1];
+    } else {
+        return 'OOPS';
+    }
 };
 
+//console.log(fibonacci());
 // Do not edit below this line
 module.exports = fibonacci;
