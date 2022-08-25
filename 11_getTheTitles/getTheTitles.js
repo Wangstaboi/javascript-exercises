d7b8f4cc345b81da39f9cf22d02f4dd67e5f4c57
Ryan Wang <rwryanwang@gmail.com>
2022-08-25 16:58:54 -0700

    Finished getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+        arr.push(obj[i]['title']);
+    }
+    return arr;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+  console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
