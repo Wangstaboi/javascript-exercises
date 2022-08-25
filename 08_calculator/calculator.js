43e7c004302e4fd9b6e47274d8f1689a10e41afd
Wangstaboi <bubmom9@gmail.com>
2022-08-25 15:53:40 -0700

    Finished calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	var fact = 1;
+  if (num === 0) {
+    return 1;
+  } else {
+    for (let i = num; i > 0; i--) {
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
